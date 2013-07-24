5f8c99bbf79921432adba10ae9ae0af036c23003
Laszlo Velinszky <djlancelot@freemail.hu>
2013-07-24 13:10:26 +0200

    Working sync

--- datagrepper.js
+++ datagrepper.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 var util = require('util');
-var rest = require('restler');
+var httpsync = require('httpsync');
 var fs = require('fs');
 var cheerio = require('cheerio');
-var join = require('join');
-
-var fns = join.create();
 
 var places = [];
 
@@ -15,47 +12,36 @@ var loadSource = function(){
     return JSON.parse(fs.readFileSync(datasource));
 }
 
-var buildFn = function(id, name,order, places,task){
-    var callBack = fns.add();
-    var parseIdokep = function(result){
-	if (result instanceof Error) {
-	    console.log('Error: ' + result.message);
-	}else{
-	    $ = cheerio.load(result);
-	    // 
-	    // Parse each element
-	    lastcell = $('.beszamolo0').last();
-	    elem = lastcell.text().split('\n');
-	    wa = elem[3].trim();
-	    ws = elem[4].trim();
-	    var data = { order: order, 
-			 id: id, 
-			 name: name,
-			 windSpeed: ws,
-			 windDir: wa
-		       };
-	    places.push(data);
-	    console.log(name+"\t"+ws+"\t"+wa+"\n");
-	}
-	//task.removeListener('complete',parseIdokep);
-	callBack();
-    }
-    return parseIdokep;
-}
-
 
 var data = loadSource();
-data.forEach(function(elem){
-var task = rest.get('http://www.idokep.hu/automata/'+elem.id);
-task.on('complete',
-						   buildFn(elem.id,
-							   elem.name,
-							   elem.order,
-							   places,task));
-});
 
-writedata = function(){
-    console.log(places);    
+var parseIdokep = function(result, order,id,name,palces){
+    if (result instanceof Error) {
+	console.log('Error: ' + result.message);
+    }else{
+	$ = cheerio.load(result);
+	// Parse each element
+	lastcell = $('.beszamolo0').last();
+	elem = lastcell.text().split('\n');
+	wa = elem[3].trim();
+	ws = elem[4].trim();
+	var data = { 
+	         order: order,
+		 id: id, 
+		 name: name,
+		 windSpeed: ws,
+		 windDir: wa
+	};
+	places.push(data);
+	console.log(data.name+"\t"+ws+"\t"+wa+"\n");
+    }
 }
 
-fns.when(writedata);
+
+data.forEach(function(elem){
+    var req = httpsync.get({url: "http://www.idokep.hu/automata/"+elem.id});
+    var res = req.end();
+    parseIdokep(res.data,elem.order,elem.id,elem.name,places);
+});
+
+
